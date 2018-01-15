3b21b4ef8badb1b516eb85b8b44c45cca77069cd
Majid Tahir <majid@acumenity.com>
2018-01-15 17:37:34 -0600

    clean up

--- bittrex-arbitrage.js
+++ bittrex-arbitrage.js
@@ -15,10 +15,9 @@ var kucoinPrice = 0;
 var bittrexPrice = 0;
 bittrexComplete = false;
 kucoinComplete = false;
-/**
- *  sendCustomRequest example
- */
- var USDTPairs = [
+
+
+var USDTPairs = [
 
 'BTC',
 'BCC',
@@ -26,8 +25,7 @@ kucoinComplete = false;
 'ETH'
  ]
 
- var BTCPairs = [
-
+var BTCPairs = [
 'NEO'
  ]
 
@@ -59,68 +57,27 @@ function getArbitrage(pairing, index, base){
 
   		//display the kucoin price
   		console.log(pairing + "-" + base);
-		console.log('------------------------------')
+		  console.log('------------------------------')
   		console.log("kucoin: " + kucoinData.data.lastDealPrice );  		
   		kucoinPrice = kucoinData.data.lastDealPrice
   		kucoinComplete =true;
 
   		//display the bittrex price
-		console.log( "bittrex price " + data.result[0].Last);
-		bittrexPrice = data.result[0].Last;
-		bittrexComplete = true;
+  		console.log( "bittrex price " + data.result[0].Last);
+  		bittrexPrice = data.result[0].Last;
+  		bittrexComplete = true;
 		
-		//this assumes your funds are on bittrex and you'll move it to kucoin to capture the arbitrage
-		//if your funds on kucoin, you'd flip the sign below
-		if (bittrexPrice < kucoinPrice)
-		{
-		  console.log ("Kucoin is " + (kucoinPrice - bittrexPrice) + " more than Bittrex") ;
-		  console.log("Kucoin is " + ((kucoinPrice - bittrexPrice) / bittrexPrice)*100 + "% more than Bittrex");
-		 }
-		 else{
-		  console.log('no arbitrage possible for ' + pairing);
-		 }	 
-		console.log('------------------------------') 
-	});
+  		//this assumes your funds are on bittrex and you'll move it to kucoin to capture the arbitrage
+  		//if your funds on kucoin, you'd flip the sign below
+  		if (bittrexPrice < kucoinPrice)
+  		{
+  		  console.log ("Kucoin is " + (kucoinPrice - bittrexPrice) + " more than Bittrex") ;
+  		  console.log("Kucoin is " + ((kucoinPrice - bittrexPrice) / bittrexPrice)*100 + "% more than Bittrex");
+  		 }
+  		 else{
+  		  console.log('no arbitrage possible for ' + pairing);
+  		 }	 
+  		console.log('------------------------------') 
+	 });
 }, true);
 }
-
-
-/*bittrex.sendCustomRequest( 'https://api.kucoin.com/v1/open/tick?symbol=NEO-USDT', function( data ) {
-  console.log("kucoin: " + data.data.lastDealPrice );
-  kucoinPrice = data.data.lastDealPrice
-  kucoinComplete =true;
-
-  bittrex.getmarketsummary( { market : 'USDT-NEO'}, function( data, err ) {
-  console.log( "bittrex price " + data.result[0].Last);
-  bittrexPrice = data.result[0].Last;
-  bittrexComplete = true;
-
-  if (bittrexPrice < kucoinPrice)
-  {
-  	console.log ("price difference = " + (kucoinPrice - bittrexPrice));
-  	console.log("arbitrage difference = " + ((kucoinPrice - bittrexPrice) / bittrexPrice)*100);
-  }
-  else{
-  	console.log('no arbitrage possible for NEO');
-  }
-
-  
-});
-
-}, true);
-*/
-
-/**
- *  getmarkethistory example
- */
-
-
-
-/**
- *  getorderbook example
- */
-/*bittrex.getorderbook( { market : 'BTC-PIVX', depth : 10, type : 'both' }, function( data ) {
-
-    data.result.buy.forEach(function(dataset) { console.log(dataset); });
-    data.result.sell.forEach(function(dataset) { console.log(dataset); });
-});*/
